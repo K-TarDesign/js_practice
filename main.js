455d7aea7095904b9d30a4bd2d2318d925a1cb36
Kari T. <131820486+K-TarDesign@users.noreply.github.com>
2025-02-03 13:56:40 -1000

    DOM practice

--- main.js
+++ main.js
@@ -3,5 +3,15 @@ const container = document.querySelector("#container");
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "It's Raining!";
-
 container.appendChild(content);
+
+const weather = document.createElement("div");
+weather.classList.add("update");
+weather.textContent = "Liar! Look out your frikkin' window!";
+container.appendChild(weather);
+
+const swapUp = document.createElement("button");
+swapUp.classList.add("btn");
+swapUp.textContent = "Change the Weather!!!";
+container.appendChild(swapUp);
+swapUp.onclick = () => content.textContent = "It's SUNNY!";
