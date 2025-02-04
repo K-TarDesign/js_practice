a277c75c66de776d00843aaa51e99cad2afa57ff
Kari T. <131820486+K-TarDesign@users.noreply.github.com>
2025-02-04 10:34:09 -1000

    DOM practice

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 const container = document.querySelector("#container");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "It's Raining!";
-container.appendChild(content);
+const para = document.createElement("p");
+para.textContent = "Hey. I'm red!";
+para.setAttribute("style", "color: red");
+container.appendChild(para);
 
-const weather = document.createElement("div");
-weather.classList.add("update");
-weather.textContent = "Liar! Look out your frikkin' window!";
-container.appendChild(weather);
+const head3 = document.createElement("h3");
+head3.textContent = "I'm a blue h3!";
+head3.setAttribute("style", "color: blue");
+container.appendChild(head3);
 
-const swapUp = document.createElement("button");
-swapUp.classList.add("btn");
-swapUp.textContent = "Change the Weather!!!";
-container.appendChild(swapUp);
-swapUp.onclick = () => content.textContent = "It's SUNNY!";
+const divider = document.createElement("div");
+divider.setAttribute("style", "border: 2px solid black; background-color: pink");
+container.appendChild(divider);
+
+    const divH1 = document.createElement("h1");
+    divH1.textContent = "I'm in a div";
+    divider.appendChild(divH1);
+    
+    const divPara = document.createElement("p");
+    divPara.textContent = "ME TOO!";
+    divider.appendChild(divPara);
+
+    const btnSwitch = document.createElement("button");
+    btnSwitch.classList.add("btn");
+    btnSwitch.textContent = "I'm a button! Press Me!";
+    divider.appendChild(btnSwitch);
+
+    btnSwitch.onclick = () => container.removeChild(container.firstElementChild);
+
