5e858cf6387a707c2e668d16d5f074925f9b450a
Kari T. <131820486+K-TarDesign@users.noreply.github.com>
2025-02-05 10:14:18 -1000

    Practice DOM Events

--- main.js
+++ main.js
@@ -1,31 +1,13 @@
 const container = document.querySelector("#container");
 
-const para = document.createElement("p");
-para.textContent = "Hey. I'm red!";
-para.setAttribute("style", "color: red");
-container.appendChild(para);
+const box = document.createElement("div");
+box.classList.add("box");
+box.setAttribute("style", "height:1em; width:1em; border: 1px solid black; background-color:teal");
+container.appendChild(box);
 
-const head3 = document.createElement("h3");
-head3.textContent = "I'm a blue h3!";
-head3.setAttribute("style", "color: blue");
-container.appendChild(head3);
-
-const divider = document.createElement("div");
-divider.setAttribute("style", "border: 2px solid black; background-color: pink");
-container.appendChild(divider);
-
-    const divH1 = document.createElement("h1");
-    divH1.textContent = "I'm in a div";
-    divider.appendChild(divH1);
-    
-    const divPara = document.createElement("p");
-    divPara.textContent = "ME TOO!";
-    divider.appendChild(divPara);
-
-    const btnSwitch = document.createElement("button");
-    btnSwitch.classList.add("btn");
-    btnSwitch.textContent = "I'm a button! Press Me!";
-    divider.appendChild(btnSwitch);
-
-    btnSwitch.onclick = () => container.removeChild(container.firstElementChild);
+const addBox = document.createElement("button");
+addBox.classList.add("btn");
+addBox.textContent = "Add Box";
+addBox.addEventListener("click", () => container.insertBefore(box, addBox));
+container.appendChild(addBox);
 
