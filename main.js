454f157269cd272cde4dfba3541e57edd964bab5
Kari T. <131820486+K-TarDesign@users.noreply.github.com>
2025-02-07 09:13:25 -1000

    Practice DOM events

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const container = document.querySelector("#container");
 
 const box = document.createElement("div");
-box.classList.add("box");
-box.setAttribute("style", "height:1em; width:1em; border: 1px solid black; background-color:teal");
-container.appendChild(box);
+    box.classList.add("box");
+    box.addEventListener("mouseover", () => box.classList.add("shift"));
 
 const addBox = document.createElement("button");
-addBox.classList.add("btn");
-addBox.textContent = "Add Box";
-addBox.addEventListener("click", () => container.insertBefore(box, addBox));
+    addBox.classList.add("btn");
+    addBox.textContent = "Add Box";
+    addBox.addEventListener("click", () => container.insertBefore(box, addBox));
+
 container.appendChild(addBox);
+
+const container2 = document.createElement("div");
+    container.appendChild(container2);
+
+const megaBox = document.createElement("div");
+    megaBox.classList.add("box", "mega");
+    megaBox.addEventListener("click", () => megaBox.classList.add("shift"));
+
+container2.appendChild(megaBox);
+
+
 
