e623994d40945d8866a18c243d06db4f394eda9e
Kari T. <131820486+K-TarDesign@users.noreply.github.com>
2025-02-11 13:17:40 -1000

    Practice box creation

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
 const container = document.querySelector("#container");
 
-const box = document.createElement("div");
-    box.classList.add("box");
-    box.addEventListener("mouseover", () => box.classList.add("shift"));
+const boxes = document.createElement("div");
+    boxes.classList.add("box");
+    boxes.addEventListener("mouseover", () => boxes.classList.add("shift"));
 
-const addBox = document.createElement("button");
-    addBox.classList.add("btn");
-    addBox.textContent = "Add Box";
-    addBox.addEventListener("click", () => container.insertBefore(box, addBox));
 
-container.appendChild(addBox);
-
-const container2 = document.createElement("div");
-    container.appendChild(container2);
-
-const megaBox = document.createElement("div");
-    megaBox.classList.add("box", "mega");
-    megaBox.addEventListener("click", () => megaBox.classList.add("shift"));
-
-container2.appendChild(megaBox);
+function createBoxes(boxCount) {
 
 
 
+}
+
+function boxInputCheck(value) {
+
+}
