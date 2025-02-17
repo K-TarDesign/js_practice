d0487d0b75ac3614c88e25c855b1018a4308a91b
Kari T. <131820486+K-TarDesign@users.noreply.github.com>
2025-02-16 15:23:38 -1000

    Practice: verify entry

--- main.js
+++ main.js
@@ -4,13 +4,39 @@ const boxes = document.createElement("div");
     boxes.classList.add("box");
     boxes.addEventListener("mouseover", () => boxes.classList.add("shift"));
 
+let boxCount = document.querySelector("#boxCount");
 
-function createBoxes(boxCount) {
+
+function boxInputCheck(value) {
+    let confirmedValue;
+
+    if (typeof value !== number) {
+        alert("Your entry is not a number.");
+        break;
+    } else if (value < 1) {
+        alert("The value you have entered is too low.");
+        break;
+    } else if (value > 50) {
+        alert("The value you entered is too high.");
+        break;
+    } else {
+        alert(`You have chosen ${value} rows & ${value} columns.`);
+        confirmedValue = value;
+    }
+
+    boxCount.textContent = ""
+    return boxCount = confirmedValue
+}
+
+function createRow(rows) {
+    let numberRows = rows;
 
 
 
 }
 
-function boxInputCheck(value) {
+function createColumns(columns) {
+    let numberColumns = columns;
+
 
 }
